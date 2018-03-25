e5484bb3a61c71f40f34f9521905a16ad79d83c3
Kjell-Morten <post@kjellmorten.no>
2018-03-25 16:36:29 +0200

    Test updating an existing document

--- tests/set-test.js
+++ tests/set-test.js
@@ -3,6 +3,7 @@ import {
   dbUri,
   openMongoWithCollection,
   closeMongo,
+  insertDocument,
   getDocuments,
   deleteDocuments
 } from './helpers/mongo'
@@ -74,4 +75,40 @@ test('should set array of documents', async (t) => {
   t.is(docs.length, 2)
   t.true(docs.some(doc => doc.id === 'ent1'))
   t.true(docs.some(doc => doc.id === 'ent2'))
+})
+
+test('should update existing document', async (t) => {
+  const {collection, collectionName} = t.context
+  await insertDocument(collection, {
+    id: 'ent1',
+    type: 'entry',
+    title: 'Entry 1',
+    theOld: true
+  })
+  const request = {
+    action: 'SET',
+    data: {
+      type: 'entry',
+      id: 'ent1',
+      title: 'Updated entry 1',
+      theNew: true
+    },
+    endpoint: {
+      collection: collectionName,
+      db: 'test'
+    }
+  }
+
+  const connection = await adapter.connect({sourceOptions})
+  const response = await adapter.send(request, connection)
+  await adapter.disconnect(connection)
+
+  t.truthy(response)
+  t.is(response.status, 'ok')
+  const docs = await getDocuments(collection, {type: 'entry'})
+  t.is(docs.length, 1)
+  t.is(docs[0].id, 'ent1')
+  t.true(docs[0].theNew)
+  t.true(docs[0].theOld)
+  t.is(docs[0].title, 'Updated entry 1')
 })
