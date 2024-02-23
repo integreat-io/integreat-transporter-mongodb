371bfb2f384bc45dbbb3c52b37fc86a593ea19ad
Kjell-Morten <post@kjellmorten.no>
2024-02-23 12:02:22 +0100

    Update dev env to tsimp

--- ava.config.js
+++ ava.config.js
@@ -2,10 +2,14 @@ import commonConfig from './ava-common.config.js'
 
 export default {
   ...commonConfig,
+  environmentVariables: {
+    ...commonConfig.environmentVariables,
+    TSIMP_DIAG: 'ignore',
+  },
   extensions: { ts: 'module' },
   nodeArguments: ['--import=tsimp'],
   watchMode: {
-    ignoreChanges: ['{coverage,dist,media}/**', 'mongodb/**', '**/*.md'],
+    ignoreChanges: ['{coverage,dist,media,.tsimp}/**', 'mongodb/**', '**/*.md'],
   },
   files: ['src/**/*.test.ts'],
 }
